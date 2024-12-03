81ced71eded2a4378bcd0595c3c2026b4a2240ac
Denis <denisatanasov76@gmail.com>
2024-12-03 14:19:55 +0200

    create home, register and login components

--- my-furniture-store/src/app/app.routes.ts
+++ my-furniture-store/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
